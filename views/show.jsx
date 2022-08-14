7933a24346b44f1634a80f5f95231f05ea0ec24a
David Fischer <dfischer@fischerltd.com>
2022-08-14 13:51:20 -0400

    commit

--- views/show.jsx
+++ views/show.jsx
@@ -7,6 +7,7 @@ function Show({ bread, index }) {
     return (
         <Default>
             <h3>{bread.name}</h3>
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />

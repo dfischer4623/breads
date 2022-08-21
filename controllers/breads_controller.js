a01e13216e514eeba43a27a15e0cf6f0be87eabb
David Fischer <dfischer@fischerltd.com>
2022-08-21 12:01:35 -0400

    mongoose

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,7 +6,6 @@ const Bread = require('../models/bread.js')
 breads.get('/', (req, res) => {
   Bread.find()
     .then(foundBreads => {
-      // console.log(foundBreads)
       res.render('index', {
         breads: foundBreads,
         title: 'Index Page'
@@ -43,7 +42,6 @@ breads.get('/:id/edit', (req, res) => {
     })
 })
 
-
 // DELETE
 breads.delete('/:id', (req, res) => {
   Bread.findByIdAndDelete(req.params.id)
@@ -61,7 +59,6 @@ breads.put('/:id', (req, res) => {
   }
   Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then(updatedBread => {
-      console.log(updatedBread)
       res.redirect(`/breads/${req.params.id}`)
     })
 })
@@ -79,5 +76,32 @@ breads.get('/:id', (req, res) => {
     })
 })
 
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany([
+    {
+      name: 'Rye',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'French',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'Gluten Free',
+      hasGluten: false,
+      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'Pumpernickel',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    }
+  ])
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
 
 module.exports = breads

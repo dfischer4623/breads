2a4b0d3694b7e3959b547ccaa92f1412dd2e7079
David Fischer <dfischer@fischerltd.com>
2022-08-21 11:39:02 -0400

    mongoose

--- views/show.jsx
+++ views/show.jsx
@@ -7,9 +7,9 @@ function Show({ bread, index }) {
     return (
         <Default>
             <h3>{bread.name}</h3>
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
    

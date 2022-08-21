462db48be08224ee477588d03ea4add8624456aa
David Fischer <dfischer@fischerltd.com>
2022-08-21 10:19:01 -0400

    mongoose

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
-
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // CONFIGURATION
 require('dotenv').config()

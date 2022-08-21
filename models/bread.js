12e2e5a1bc9d56d825f0555f9b42ed129182688b
David Fischer <dfischer@fischerltd.com>
2022-08-21 13:52:22 -0400

    mongoose

--- models/bread.js
+++ models/bread.js
@@ -14,6 +14,11 @@ const breadSchema = new Schema({
   }
 })
 
+// helper methods 
+breadSchema.methods.getBakedBy = function(){
+  return `${this.name} was baked with love by ${this.baker}`
+}
+
 const Bread = mongoose.model('Bread', breadSchema)
 
 module.exports = Bread
